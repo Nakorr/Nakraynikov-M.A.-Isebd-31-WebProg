17d578f89a34e33dac287dd5f8360e78252795af
Nakorr <Nakorr777@gmail.com>
2019-10-11 10:04:39 +0400

    Laba2-3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
+import {Routes, RouterModule} from '@angular/router';
 import { AppComponent } from './app.component';
+import { ServiceComponent } from './service/service.component';
+import { MyAdComponent } from './my-ad/my-ad.component';
+import { AdminComponent } from './admin/admin.component';
+
+const appRoutes: Routes =[
+    { path: 'service', component: ServiceComponent},
+    { path: 'myad', component: MyAdComponent},
+    { path: 'admin', component: AdminComponent},
+];
 
 @NgModule({
+
   declarations: [
-    AppComponent
+    AppComponent,
+    ServiceComponent,
+    MyAdComponent,
+    AdminComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
-  ],
+    RouterModule.forRoot(appRoutes)],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ src/app/my-ad/my-ad.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-my-ad',
+  templateUrl: './my-ad.component.html',
+  styleUrls: ['./my-ad.component.sass']
+})
+export class MyAdComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
